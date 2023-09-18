f98a2b026679a104e95a886c62a7af5ae96c7f75
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-09-19 11:44:40 +1200

    Attempts at movement code

--- Levels/Player.gd
+++ Levels/Player.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 class_name Player
 
-
-var speed := 500.0
-var jump_impulse := 1200.0
-var base_gravity := 4000.0
-
-var _velocity := Vector2.ZERO
-
+@export var jump_speed = 275
+var falling = false
+var direction = Vector2.RIGHT
+var speed = 500.0
+var jump_impulse = 1200.0
+var gravity = 4000.0
 
 func _physics_process(delta: float) -> void:
-	# Starting with input.
-	var input_direction_x: float = (
-		Input.get_action_strength("ui_right")
-		- Input.get_action_strength("ui_left")
-	)
-	var is_jumping := is_on_floor() and Input.is_action_just_pressed("ui_accept")
+	var move_direction = 0
+	if Input.is_action_pressed("moveleft"):
+		move_direction -= 1
+		direction = Vector2.LEFT
+	if Input.is_action_pressed("moveright"):
+		move_direction += 1
+		direction = Vector2.RIGHT
 
-	# Calculating horizontal velocity.
-	_velocity.x = input_direction_x * speed
-	# Calculating vertical velocity.
-	_velocity.y += base_gravity * delta
-	if is_jumping:
-		_velocity.y = -jump_impulse
-	# Moving the character.
+	velocity.x = move_direction * speed
+	set_velocity(velocity)
+	set_up_direction(Vector2.UP)
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_speed
+		falling = true
+
+	if is_on_floor():
+		falling = false
+
+	velocity.y += gravity * delta
