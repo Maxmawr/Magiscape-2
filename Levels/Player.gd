8e386ada96935ad6807a3813e4252d3c599b495d
Maxmawr <116230043+Maxmawr@users.noreply.github.com>
2023-09-20 19:47:12 +1200

    Got movement working and start of different colours

--- Levels/Player.gd
+++ Levels/Player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 class_name Player
 
+@export var colour = "blue"
 @export var jump_speed = 275
 var falling = false
 var direction = Vector2.RIGHT
-var speed = 500.0
-var jump_impulse = 1200.0
-var gravity = 4000.0
+var speed = 125
+var jump_impulse = 200
+var gravity = 800.0
 
-func _physics_process(delta: float) -> void:
+func _ready():
+	print("hello")
+
+func _physics_process(delta):
 	var move_direction = 0
 	if Input.is_action_pressed("moveleft"):
 		move_direction -= 1
@@ -19,14 +23,30 @@ func _physics_process(delta: float) -> void:
 
 	velocity.x = move_direction * speed
 	set_velocity(velocity)
-	set_up_direction(Vector2.UP)
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -jump_speed
+		velocity.y = -jump_impulse
 		falling = true
 
 	if is_on_floor():
 		falling = false
 
 	velocity.y += gravity * delta
+
+func _process(_delta):
+	if not is_on_floor():
+		$AnimationPlayer.play(colour + "_jump")
+		if Input.is_action_pressed("moveleft"):
+			$sprite.set_flip_h(true)
+		elif Input.is_action_pressed("moveright"):
+			$sprite.set_flip_h(false)
+	elif Input.is_action_pressed("moveleft"):
+		$AnimationPlayer.play(colour + "_walk")
+		$sprite.set_flip_h(true)
+	elif Input.is_action_pressed("moveright"):
+		$AnimationPlayer.play(colour + "_walk")
+		$sprite.set_flip_h(false)
+	else:
+		$AnimationPlayer.stop()
+		$AnimationPlayer.play(colour + "_idle")
