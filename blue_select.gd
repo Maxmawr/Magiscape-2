8022246ffc368e761131a0ec374cbe25dd623836
Maxmawr <116230043+Maxmawr@users.noreply.github.com>
2023-09-23 21:12:56 +1200

    Added buttons to choose character colour

---
+++ blue_select.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+func _on_mouse_entered():
+	$AnimationPlayer.play("grow")
+
+
+func _on_mouse_exited():
+	$AnimationPlayer.play("shrink")
+
+
+func _on_pressed():
+	Permavariables.colour = "blue"
+	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
