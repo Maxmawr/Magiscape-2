d938bd379bc52eab81077ac3ef610d87b7fd646b
Maxmawr <116230043+Maxmawr@users.noreply.github.com>
2023-10-13 20:26:31 +1300

    Changed jump height

--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var ground_speed := 500.0
 var air_speed := 300.0
-var jump_height := 50.0
+var jump_height := 250.0
 var speed := 200.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var colour = Permavariables.colour
