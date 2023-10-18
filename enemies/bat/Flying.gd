215d5cf10f8c72d3b359f9f30f5815bb6c37ddf0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-10-19 12:44:51 +1300

    more work on the bat

--- enemies/bat/Flying.gd
+++ enemies/bat/Flying.gd
@@ -1,14 +1,23 @@
 extends State
 
-@export var speed = 10
+var thispos
+@onready var lastpos = owner.progress
+
+@onready var animationplayer = get_parent().get_parent().get_node("AnimationPlayer")
+
+@export var speed = 0.5
 
 
 func enter(_msg := {}) -> void:
-	#flying animation here
-	pass
+	animationplayer.play("flying")
 
 func update(delta: float) -> void:
-	pass
+	thispos = owner.progress
+	if thispos > lastpos:
+		owner.get_node("sprite").flip_h = false
+	else:
+		owner.get_node("sprite").flip_h = true
+	lastpos = thispos
 
 func physics_update(delta: float) -> void:
-	owner.progress_ratio += speed * 1
+	owner.progress_ratio += delta * speed
