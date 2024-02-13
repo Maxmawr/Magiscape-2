a09e165305c313cf745384b2d9ec9be3bf143ba6
Maxmawr <116230043+Maxmawr@users.noreply.github.com>
2024-02-13 22:36:47 +0000

    Started doing Gazer Laser

--- enemies/gazer/Hostile.gd
+++ enemies/gazer/Hostile.gd
@@ -1,10 +1,20 @@
 extends State
 
+@onready var player = owner.get_parent().get_node("player")
+var line_color = Color(1, 0, 0) # Red color for the line
+var inside
+
 func enter(_msg := {}) -> void:
-	pass
+	inside = true
 
 func update(_delta: float) -> void:
 	pass
 
 func physics_update(_delta: float) -> void:
 	pass
+
+func _on_gazer_draw():
+	if inside == true:
+		var player_position = player.global_position
+		var enemy_position = owner.get_global_position()
+		owner.draw_line(Vector2.ZERO, player_position - enemy_position, line_color)
