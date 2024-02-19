dfb12fc7144baf873e48fdf56f445d9a233ee7fd
Maxmawr <116230043+Maxmawr@users.noreply.github.com>
2024-02-20 10:03:41 +1300

    general changes

--- enemies/gazer/gazer_laser.gd
+++ enemies/gazer/gazer_laser.gd
@@ -19,3 +19,7 @@ func _physics_process(delta):
 func _on_body_entered(body):
 	if body.name == "player":
 		body.take_damage(8)
+	
+	if body.is_in_group("enemies"):
+		body.handle_hit(8)
+		print("OJNWAUGHUOIWAH")
