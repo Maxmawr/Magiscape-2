cf6a515f830b902dd2a4ff04dd0c1704a8229e6e
Maxmawr <116230043+Maxmawr@users.noreply.github.com>
2024-03-19 21:46:15 +1300

    re-added door

--- Levels/door.gd
+++ Levels/door.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	if body.name == "player":
-		get_tree().change_scene_to_file("res://Levels/final_screen.tscn")
+	if body.name == "player" and get_parent().name == "Level 1":
+		get_tree().change_scene_to_file("res://Levels/level_2.tscn")
