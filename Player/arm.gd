87e64f86f69b9f72a479993c32b5c764629e6301
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-11 07:45:50 +1200

    adding arm animation support to all colours

--- Player/arm.gd
+++ Player/arm.gd
@@ -1,5 +1,18 @@
 extends Sprite2D
 
+func _ready():
+	if Permavariables.colour == "blue":
+		frame = 0
+	elif Permavariables.colour == "brown":
+		frame = 1
+	elif Permavariables.colour == "dark":
+		frame = 2
+	elif Permavariables.colour == "green":
+		frame = 3
+	elif Permavariables.colour == "red":
+		frame = 4
+	elif Permavariables.colour == "yellow":
+		frame = 5
 func _process(_delta):
 	look_at(get_global_mouse_position())
 	rotation_degrees -= 90
