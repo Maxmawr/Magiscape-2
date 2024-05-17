db301835b2826774a635b4dc98a78353d36b2067
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-17 14:15:39 +1200

    fixed jump animation for all colors

--- Player/arm_control.gd
+++ Player/arm_control.gd
@@ -7,7 +7,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	#if Permavariables.direction_facing == "left":
+		#position.x = 1.5
+		#position.y = -0.5
+	#else:
+		#
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
