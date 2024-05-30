33c1dcf1257bbe13b5100a87e80414bc3a38caec
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-30 12:40:18 +1200

    cont from prev commit

--- enemies/goblin/viewcone.gd
+++ enemies/goblin/viewcone.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 
-func _process(delta):
+func _process(_delta):
 	if owner.direction_facing == "right":
-		scale.x = -1
-	elif owner.direction_facing == "right":
 		scale.x = 1
+	elif owner.direction_facing == "right":
+		scale.x = -1
