9ab5e3d3dda5d3660931ee5cf714609b01484398
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-02 12:05:02 +1200

    started making keybind menu, loading screen finished

--- GUI/loading_screen.gd
+++ GUI/loading_screen.gd
@@ -5,22 +5,11 @@ var current_pos = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://GUI/main_menu.tscn")
-	#ParticlesCache.load_particles()
-	#get_tree().change_scene_to_file("res://GUI/main_menu.tscn")
+	print(materials_size)
 
 func _process(delta: float) -> void:
-		for material in ParticlesCache.materials:
-			if current_pos == materials_size:
-				get_tree().change_scene_to_file("res://GUI/main_menu.tscn")
-			else:
-				var particles_instance = GPUParticles2D.new()
-				particles_instance.set_process_material(material)
-				particles_instance.set_one_shot(true)
-				particles_instance.set_modulate(Color(1,1,1,0))
-				particles_instance.set_emitting(true)
-				self.add_child(particles_instance)
-				current_pos += 1
-				print(current_pos)
-				$TextureProgressBar.value = int((float(current_pos) / materials_size) * 100)
- 
+	ParticlesCache.load_particles(current_pos)
+	current_pos += 1
+	$TextureProgressBar.value = int((float(current_pos) / materials_size) * 100)
+	if current_pos == materials_size:
+		get_tree().change_scene_to_file("res://GUI/main_menu.tscn")
--- ParticlesCache.gd
+++ ParticlesCache.gd
@@ -22,13 +22,10 @@ var materials = [
 #var current_pos = 0
 #
 #
-#func load_particles():
-	#for material in materials:
-		#var particles_instance = GPUParticles2D.new()
-		#particles_instance.set_process_material(material)
-		#particles_instance.set_one_shot(true)
-		#particles_instance.set_modulate(Color(1,1,1,0))
-		#particles_instance.set_emitting(true)
-		#self.add_child(particles_instance)
-		#current_pos += 1
-		#print(current_pos)
+func load_particles(num):
+	var particles_instance = GPUParticles2D.new()
+	particles_instance.set_process_material(materials[num])
+	particles_instance.set_one_shot(true)
+	particles_instance.set_modulate(Color(1,1,1,0))
+	particles_instance.set_emitting(true)
+	self.add_child(particles_instance)
